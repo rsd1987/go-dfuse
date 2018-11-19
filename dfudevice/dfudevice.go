f541462a5d8ea89a8d211a3efdc054237c5e5ad2
Will Toth <toth@revrobotics.com>
2018-11-18 20:51:05 -0600

    More complete of dfu file read

--- dfudevice/dfudevice.go
+++ dfudevice/dfudevice.go
@@ -140,7 +140,11 @@ func computeCRC() {
 }
 
 func Test(filename string) {
-	readDFUFile(filename)
+	_, err := ReadDFUFile(filename)
+
+	if err != nil {
+		fmt.Println("DFU File Format Failed: %v", err)
+	}
 }
 
 type DFUTarget struct {
@@ -178,19 +182,20 @@ type DFUFile struct {
 		Product       uint16
 		Vendor        uint16
 		DfuFormat     uint16
-		Ufd           string
+		Ufd           [3]byte
 		Length        uint8
 		Crc32         uint32
 	}
 }
 
-func readDFUFile(filename string) (DFUFile, error) {
+func ReadDFUFile(filename string) (DFUFile, error) {
 	var fileData DFUFile
 
 	fileHandle, err := os.Open(filename)
 	defer fileHandle.Close()
 
-	fmt.Println(filename)
+	//TODO: Verbose output
+	//fmt.Println(filename)
 
 	if err != nil {
 		return fileData, err
@@ -204,17 +209,26 @@ func readDFUFile(filename string) (DFUFile, error) {
 	err = binary.Read(fileHandle, binary.LittleEndian, &fileData.Prefix)
 
 	if err != nil {
-		fmt.Printf("Error: %v\r\n", err)
 		return fileData, err
 	}
 
-	fmt.Printf("Signature: %x, v%d, image size: %d, targets: %d\r\n",
-		fileData.Prefix.Signature,
-		fileData.Prefix.Version,
-		fileData.Prefix.Size,
-		fileData.Prefix.Targets)
+	if string(fileData.Prefix.Signature[:]) != "DfuSe" {
+		return fileData, fmt.Errorf("Error in image prefix, dfu file failed")
+	}
 
-	for targets := uint8(0); targets < fileData.Prefix.Targets; targets++ {
+	//TODO: Verbose output
+	//fmt.Printf("Signature: %x, v%d, image size: %d, targets: %d\r\n",
+	//	fileData.Prefix.Signature,
+	//	fileData.Prefix.Version,
+	//	fileData.Prefix.Size,
+	//	fileData.Prefix.Targets)
+
+	fileData.Images = make([]DFUImage, fileData.Prefix.Targets)
+
+	for imageIdx := range fileData.Images {
+
+		image := &fileData.Images[imageIdx]
+
 		// Decode the Image Prefix
 		//   <   little endian
 		//   6s      char[6]     signature   "Target"
@@ -223,116 +237,73 @@ func readDFUFile(filename string) (DFUFile, error) {
 		//   255s    char[255]   name        name of the target
 		//   I       uint32_t    size        size of image (not incl prefix)
 		//   I       uint32_t    elements    Number of elements in the image
-		var image DFUImage
 		err = binary.Read(fileHandle, binary.LittleEndian, &image.Prefix)
 
 		if err != nil {
-			fmt.Printf("Error: %v\r\n", err)
 			return fileData, err
 		}
 
-		fmt.Printf("Signature: %x, num: %d, alt settings: %d, name: %s, size: %d, elements: %d\r\n",
-			image.Prefix.Signature,
-			image.Prefix.IsNamed,
-			image.Prefix.AltSetting,
-			image.Prefix.Name,
-			image.Prefix.Size,
-			image.Prefix.Elements)
+		if string(image.Prefix.Signature[:]) != "Target" {
+			return fileData, fmt.Errorf("Error in image prefix, dfu file failed")
+		}
+
+		//TODO: Verbose output
+		//fmt.Printf("Signature: %x, num: %d, alt settings: %d, name: %s, size: %d, elements: %d\r\n",
+		//	image.Prefix.Signature,
+		//	image.Prefix.IsNamed,
+		//	image.Prefix.AltSetting,
+		//	image.Prefix.Name,
+		//	image.Prefix.Size,
+		//	image.Prefix.Elements)
 
 		image.Targets = make([]DFUTarget, image.Prefix.Elements)
 
-		for idx := range image.Targets {
+		for targetIdx := range image.Targets {
 
 			// Decode target prefix
 			//   <   little endian
 			//   I   uint32_t    element address
 			//   I   uint32_t    element size
-			err = binary.Read(fileHandle, binary.LittleEndian, &image.Targets[idx].Prefix)
+			err = binary.Read(fileHandle, binary.LittleEndian, &image.Targets[targetIdx].Prefix)
 			if err != nil {
-				fmt.Printf("Error: %v\r\n", err)
 				return fileData, err
 			}
 
-			image.Targets[idx].Elements = make([]byte, image.Targets[idx].Prefix.Size)
-			_, err = fileHandle.Read(image.Targets[idx].Elements)
+			image.Targets[targetIdx].Elements = make([]byte, image.Targets[targetIdx].Prefix.Size)
+			_, err = fileHandle.Read(image.Targets[targetIdx].Elements)
 
 			if err != nil {
-				fmt.Printf("Error: %v\r\n", err)
 				return fileData, err
 			}
 		}
 	}
 
+	//   <   little endian
+	//   H   uint16_t    device  Firmware version
+	//   H   uint16_t    product
+	//   H   uint16_t    vendor
+	//   H   uint16_t    dfu     0x11a   (DFU file format version)
+	//   3s  char[3]     ufd     'UFD'
+	//   B   uint8_t     len     16
+	//   I   uint32_t    crc32
+	err = binary.Read(fileHandle, binary.LittleEndian, &fileData.Suffix)
+
+	//TODO: Verbose output
+	//fmt.Printf("version: %x, product: %04x, vendor: %04x, format: %d, length: %d, crc32: %d\r\n",
+	//	fileData.Suffix.DeviceVersion,
+	//	fileData.Suffix.Product,
+	//	fileData.Suffix.Vendor,
+	//	fileData.Suffix.DfuFormat,
+	//	fileData.Suffix.Length,
+	//	fileData.Suffix.Crc32)
+
+	if string(fileData.Suffix.Ufd[:]) != "UFD" {
+		return fileData, fmt.Errorf("Error in suffix prefix, dfu file failed")
+	}
+
+	//TODO: check CRC
+
 	return fileData, nil
-
-	/*
-		    for target_idx in range(dfu_prefix['targets']):
-				// Decode the Image Prefix
-
-		        // <6sBI255s2I
-		        //   <   little endian
-		        //   6s      char[6]     signature   "Target"
-		        //   B       uint8_t     altsetting
-		        //   I       uint32_t    named       bool indicating if a name was used
-		        //   255s    char[255]   name        name of the target
-		        //   I       uint32_t    size        size of image (not incl prefix)
-		        //   I       uint32_t    elements    Number of elements in the image
-		        img_prefix, data = consume('<6sBI255s2I', data,
-		                                   'signature altsetting named name '
-		                                   'size elements')
-		        img_prefix['num'] = target_idx
-		        if img_prefix['named']:
-		            img_prefix['name'] = cstring(img_prefix['name'])
-		        else:
-		            img_prefix['name'] = ''
-		        print('    %(signature)s %(num)d, alt setting: %(altsetting)s, '
-		              'name: "%(name)s", size: %(size)d, elements: %(elements)d'
-		              % img_prefix)
-
-		        target_size = img_prefix['size']
-		        target_data, data = data[:target_size], data[target_size:]
-		        for elem_idx in range(img_prefix['elements']):
-		            // Decode target prefix
-		            //   <   little endian
-		            //   I   uint32_t    element address
-		            //   I   uint32_t    element size
-		            elem_prefix, target_data = consume('<2I', target_data, 'addr size')
-		            elem_prefix['num'] = elem_idx
-		            print('      %(num)d, address: 0x%(addr)08x, size: %(size)d'
-		                  % elem_prefix)
-		            elem_size = elem_prefix['size']
-		            elem_data = target_data[:elem_size]
-		            target_data = target_data[elem_size:]
-		            elem_prefix['data'] = elem_data
-		            elements.append(elem_prefix)
-
-		        if len(target_data):
-		            print("target %d PARSE ERROR" % target_idx)
-
-		    // Decode DFU Suffix
-		    //   <   little endian
-		    //   H   uint16_t    device  Firmware version
-		    //   H   uint16_t    product
-		    //   H   uint16_t    vendor
-		    //   H   uint16_t    dfu     0x11a   (DFU file format version)
-		    //   3s  char[3]     ufd     'UFD'
-		    //   B   uint8_t     len     16
-		    //   I   uint32_t    crc32
-		    dfu_suffix = named(struct.unpack('<4H3sBI', data[:16]),
-		                       'device product vendor dfu ufd len crc')
-		    print ('    usb: %(vendor)04x:%(product)04x, device: 0x%(device)04x, '
-		           'dfu: 0x%(dfu)04x, %(ufd)s, %(len)d, 0x%(crc)08x' % dfu_suffix)
-		    if crc != dfu_suffix['crc']:
-		        print("CRC ERROR: computed crc32 is 0x%08x" % crc)
-		        return
-		    data = data[16:]
-		    if data:
-		        print("PARSE ERROR")
-		        return
-
-		    return elements
-	*/
-
 }
 
 /*
